346c61af52584474adabf160bf24aeab1b2ca9a4
philipschwarz <philip.johann.schwarz@gmail.com>
2022-11-26 15:57:13 +0000

    add Auth algebra

--- modules/core/src/main/scala/shop/algebras/auth.scala
+++ modules/core/src/main/scala/shop/algebras/auth.scala
@@ -1,8 +1,15 @@
 package shop.algebras
 
-import shop.domain.auth.{Password, User, UserId, UserName}
+import shop.domain.auth.{ JwtToken, Password, User, UserId, UserName }
 
 trait Users[F[_]] {
   def find(username: UserName, password: Password): F[Option[User]]
   def create(userName: UserName, password: Password): F[Option[UserId]]
+}
+
+trait Auth[F[_]] {
+  def findUser(token: JwtToken): F[Option[User]]
+  def newUser(userName: UserName, password: Password): F[JwtToken]
+  def login(userName: UserName, password: Password): F[JwtToken]
+  def logout(token: JwtToken, userName: UserName): F[Unit]
 }
