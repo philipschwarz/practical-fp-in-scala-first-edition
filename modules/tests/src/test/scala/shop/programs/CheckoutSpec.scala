bfba3cdb552525c396645f0df92fccb3e0bdf8e9
philipschwarz <philip.johann.schwarz@gmail.com>
2022-12-31 14:12:22 +0100

    add functional tests "empty cart" and "unreachable payment client"

--- modules/tests/src/test/scala/shop/programs/CheckoutSpec.scala
+++ modules/tests/src/test/scala/shop/programs/CheckoutSpec.scala
@@ -1,6 +1,7 @@
 package shop.programs
 
 import cats.effect._
+import cats.effect.concurrent.Ref
 //import cats.effect.concurrent.Ref
 //import cats.implicits.{ catsSyntaxEq => _, _ }
 import retry.RetryPolicy
@@ -30,11 +31,23 @@ final class CheckoutSpec extends PureTestSuite {
         IO.pure(paymentId)
     }
 
+  val unreachableClient: PaymentClient[IO] =
+    new PaymentClient[IO] {
+      override def process(payment: Payment): IO[PaymentId] =
+        IO.raiseError(PaymentError(""))
+    }
+
   def successfulCart(cartTotal: CartTotal): ShoppingCart[IO] = new TestCart {
     override def get(userId: UserId): IO[CartTotal] =
       IO.pure(cartTotal)
     override def delete(userId: UserId): IO[Unit] = IO.unit
   }
+
+  val emptyCart: ShoppingCart[IO] =
+    new TestCart {
+      override def get(userId: UserId): IO[CartTotal] =
+        IO.pure(CartTotal(List.empty, USD(0)))
+    }
 
   def successfulOrders(orderId: OrderId): Orders[IO] = new TestOrders {
     override def create(userId: UserId, paymentId: PaymentId, items: List[CartItem], total: Money): IO[OrderId] =
@@ -46,15 +59,66 @@ final class CheckoutSpec extends PureTestSuite {
     import shop.logger.NoOp
     forAll { (uid: UserId, pid: PaymentId, oid: OrderId, ct: CartTotal, card: Card) =>
       IOAssertion {
-        new CheckoutProgram[IO](successfulClient(pid), successfulCart(ct), successfulOrders(oid), retryPolicy)
-          .checkout(uid, card)
-          .map { id =>
-            assert(id === oid)
-          }
+        new CheckoutProgram[IO](
+          successfulClient(pid),
+          successfulCart(ct),
+          successfulOrders(oid),
+          retryPolicy
+        ).checkout(uid, card)
+         .map { id =>
+           assert(id === oid)
+         }
       }
     }
   }
 
+  test("empty cart"){
+    implicit val bg = shop.background.NoOp
+    import shop.logger.NoOp
+    forAll { (uid: UserId, pid: PaymentId, oid: OrderId, card: Card) =>
+      new CheckoutProgram[IO](
+        successfulClient(pid),
+        emptyCart,
+        successfulOrders(oid),
+        retryPolicy
+      ).checkout(uid, card)
+        .attempt
+        .map {
+          case Left(EmptyCartError) => assert(true)
+          case _ => fail("Cart was not empty as expected")
+        }
+    }
+  }
+
+  test("unreachable payment client"){
+    forAll { (uid: UserId, cartTotal: CartTotal, oid: OrderId, card: Card) =>
+      IOAssertion {
+        Ref.of[IO,List[String]](List.empty).flatMap { logs =>
+          implicit val bg = shop.background.NoOp
+          implicit val logger = shop.logger.acc(logs)
+          new CheckoutProgram[IO](
+            unreachableClient,
+            successfulCart(cartTotal),
+            successfulOrders(oid),
+            retryPolicy
+          )
+          .checkout(uid, card)
+          .attempt
+          .flatMap {
+            case Left(PaymentError(_)) =>
+              logs.get.map {
+                case x :: xs =>
+                  assert(x.contains("Giving up") &&
+                    xs.size === MaxRetries)
+                case Nil =>
+                  fail(s"Expected $MaxRetries retries")
+              }
+            case _ => fail("Expected payment error")
+          }
+        }
+      }
+    }
+  }
 }
 
 protected class TestOrders() extends Orders[IO] {
